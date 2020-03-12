995a8bd742e3a637f11a5187de858a90b10854bc
dessale4 <dessale4@gmail.com>
2020-03-11 23:15:11 -0500

    Project structure setup

---
+++ src/main/java/edu/mum/cs/cs425/studentproject/model/AccountType.java
@@ -0,0 +1,9 @@
+package edu.mum.cs.cs425.studentproject.model;
+
+import javax.persistence.Entity;
+
+//@Entity
+public class AccountType {
+	private Long accountTypeId;
+	private String accountTypeName;
+}
---
+++ src/main/java/edu/mum/cs/cs425/studentproject/model/Authority.java
@@ -0,0 +1,5 @@
+package edu.mum.cs.cs425.studentproject.model;
+
+public class Authority {
+
+}
